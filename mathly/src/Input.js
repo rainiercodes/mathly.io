8702050698addce3d2bc218b351385c66f64b73c
DevTaylor <ziplabdev@gmail.com>
2022-01-25 09:53:13 -0800

    input

--- mathly/src/Input.js
+++ mathly/src/Input.js
@@ -10,27 +10,20 @@ export default function Input(props) {
         event.preventDefault()
         console.log('sent!')
     }
-    function auto_grow(element) {
-        element.style.height = "5px";
-        element.style.height = (element.scrollHeight)+"px";
-    }
 
     return (
         <div className="input-container">
             <form onSubmit={send} className="form">
                 <label className="label">
-                    {/* <input
+                    <input
                         className="input"
                         type="text"
                         value={message}
                         onChange={e => setMessage(e.target.value)}
-                    /> */}
-                    <textarea oninput="auto_grow(this)" className = "input" name="" id="" cols="30" value={message}
-                        onChange={e => setMessage(e.target.value)}></textarea>
+                    />
                     <div className="bottom-line"/>
                 </label>
-                
-                <input className = "send-btn" type="submit" value=""/>
+                <input className = "send-btn" type="submit" value="" />
             </form>
         </div>
     )
