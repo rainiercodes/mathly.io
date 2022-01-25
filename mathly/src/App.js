83eb386587f4ef3153fa39a7847e5162cbe7aa89
DevTaylor <ziplabdev@gmail.com>
2022-01-25 09:05:25 -0800

    form set up

---
+++ mathly/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Input from './Input.js'
+function App() {
+  return (
+    <div className="App">
+      <Input/>
+    </div>
+  );
+}
+
+export default App;
---
+++ mathly/src/Input.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import './styles/Input.css'
+export default function Input(props) {
+    
+
+
+    const [message, setMessage] = useState('')
+
+    const send = (event) => {
+        event.preventDefault()
+        console.log('sent!')
+    }
+
+    return (
+        <div className="input-container">
+            <form onSubmit={send} className="form">
+                <label className="label">
+                    <input
+                        className="input"
+                        type="text"
+                        value={message}
+                        onChange={e => setMessage(e.target.value)}
+                    />
+                    <div className="bottom-line">line</div>
+                </label>
+                <input type="submit" />
+            </form>
+        </div>
+    )
+
+
+
+
+
+
+
+
+
+
+
+
+}
